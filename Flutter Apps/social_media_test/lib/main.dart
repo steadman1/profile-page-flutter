ae3bd6752d6c309fc76d3715b6aba702db255cd8
Spencer Steadman <sjsteadman05@icloud.com>
2021-07-06 15:50:44 -0400

    profile gradient page

---
+++ Flutter Apps/social_media_test/lib/main.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_test/customnavbar.dart';
+import 'package:social_media_test/profilepage.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: MainStack(),
+      ),
+    );
+  }
+}
+
+class MainStack extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: ProfilePage("spence"),
+        ),
+        Positioned(bottom: 0, child: CustomNavBar()),
+      ],
+    );
+  }
+}
+
+class GoogleTextStyle {
+  String type;
+  Color color = Colors.white;
+  GoogleTextStyle(this.type);
+
+  static TextStyle title({Color? color}) {
+    return GoogleFonts.dmSans(
+        color: color, fontSize: 32, fontWeight: FontWeight.w600);
+  }
+
+  static TextStyle subtitle({Color? color}) {
+    return GoogleFonts.dmSans(
+        color: color, fontSize: 22, fontWeight: FontWeight.w500);
+  }
+
+  static TextStyle username({Color? color}) {
+    return GoogleFonts.dmSans(
+        color: color, fontSize: 18, fontWeight: FontWeight.w800);
+  }
+
+  static TextStyle text({Color? color}) {
+    return GoogleFonts.dmSans(
+        color: color, fontSize: 18, fontWeight: FontWeight.w400);
+  }
+}
+
+List<BoxShadow> boxShadow() => 
+[BoxShadow(
+  offset: Offset(0, 20),
+  blurRadius: 100,
+  spreadRadius: 10,
+  color: Color.fromRGBO(50, 50, 93, 0.1))];
---
+++ Flutter Apps/social_media_test/lib/profilepage.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_test/main.dart';
+import 'package:social_media_test/profilecontainerwidget.dart';
+
+class ProfilePage extends StatelessWidget {
+  final username;
+  ProfilePage(this.username);
+
+  final themeColor = Color(0xff8eb8e5);//Color(0xff9CAFB7);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [themeColor, Colors.white],
+          begin: Alignment(0, -0.8),
+          end: Alignment(0.0, -0.2),
+        ),
+      ),
+      child: Container(
+          child: ListView(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 100),
+                child: Column(
+                  children: [
+                    Row( 
+                      // crossAxisAlignment: CrossAxisAlignment.end,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Container(
+                            margin: EdgeInsets.only(top: 15),
+                            child: Text(
+                              "4497",
+                              style: GoogleTextStyle.subtitle(),
+                            )),
+                            Container(
+                            margin: EdgeInsets.only(top: 15),
+                            child: Text(
+                              "followers",
+                              style: GoogleTextStyle.text(),
+                            )),
+                          ],
+                        ),
+                        ProfileContainer(120,
+                          "https://lh3.googleusercontent.com/ogw/ADea4I7MJxPWGtpzLfTsC7242Zt5-Y_tSW7KircvjnSiwQ=s192-c-mo"),
+                        Column(
+                          children: [
+                            Container(
+                            margin: EdgeInsets.only(top: 15),
+                            child: Text(
+                              "119",
+                              style: GoogleTextStyle.subtitle(),
+                            )),
+                            Container(
+                            margin: EdgeInsets.only(top: 15),
+                            child: Text(
+                              "following",
+                              style: GoogleTextStyle.text(),
+                            )),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                        margin: EdgeInsets.only(top: 15),
+                        child: Text(
+                          "Spencer Steadman",
+                          style: GoogleTextStyle.subtitle(),
+                        )),
+                    Container(
+                        margin: EdgeInsets.only(top: 5),
+                        child: Text(
+                          "@spence",
+                          style: GoogleTextStyle.username(),
+                        ))
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 60),
+                child: Center(child: Text("developer of this app 🤌\n jrt '23 cs, xc & track\nnova swimming", 
+                textAlign: TextAlign.center,
+                style: GoogleTextStyle.text(),)),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 60),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width / 2 - 50,
+                      decoration: BoxDecoration(
+                          boxShadow: boxShadow(),
+                          borderRadius: BorderRadius.circular(15),
+                          color: themeColor),
+                      child: Center(
+                          child: Text(
+                        "Follow",
+                        style: GoogleTextStyle.text(),
+                      )),
+                    ),
+                    Container(
+                      height: 50,
+                      width: MediaQuery.of(context).size.width / 2 - 50,
+                      decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Color.fromRGBO(30, 30, 30, 0.13)),
+                          boxShadow: boxShadow(),
+                          borderRadius: BorderRadius.circular(15),
+                          color: Colors.white),
+                      child: Center(
+                          child: Text(
+                        "Message",
+                        style: GoogleTextStyle.text(),
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          )),
+    );
+  }
+}
